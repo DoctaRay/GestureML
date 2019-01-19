6baaaba95e81c7aa6a76945010ab791a17a91ba3
Raiyan Sayeed <yoboi7585@gmail.com>
2019-01-19 12:53:29 -0500

    Switched to webcam

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
+import Webcam from 'react-webcam';
 
 // import logo from './logo.svg';
 import './App.css';
@@ -48,12 +50,38 @@ class App extends Component {
     if (player) player.dispose();
   }
 
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+  //
+  // capture = () => {
+  //   console.log
+  //   const imageSrc = this.webcam.getScreenshot();
+  // };
+  capture = console.log;
+
   render() {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: "user"
+    };
     return (
       <div className="app">
         <h1>title</h1>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={this.capture}>Capture photo</button>
         <video id="recorder" className="video-js vjs-default-skin" />
-        <p>body</p>
+        <Button variant="contained" color="primary">
+          Take a picture!
+        </Button>
       </div>
     );
   }
