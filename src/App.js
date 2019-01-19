d15c0f87b792a2a9e258b11d787feb954b247135
Raiyan Sayeed <yoboi7585@gmail.com>
2019-01-19 10:38:04 -0500

    Added VideoJS libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import videojs from 'video.js';
+
+import 'videojs-record/dist/videojs.record.js';
+import 'webrtc-adapter';
+import 'recordrtc';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+const config = {
+    controls: true,
+    width: 640,
+    height: 480,
+    fluid: false,
+    plugins: {
+        record: {
+            audio: false,
+            video: true,
+            maxLength: 10,
+            debug: true
+        }
+    }
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Reference to element in the DOM tree.
+    this.recorder = React.createRef();
+  }
+
+  componentDidMount() {
+    this.player = videojs(this.recorder, config, this.handleVideoInit);
+    this.player.on('startRecord', this.handleStartRecord);
+    this.player.on('stopRecord', this.handleFinishRecord)
+  }
+
+  componentWillUnmount() {
+    const { player } = this;
+    if (player) player.dispose();
+  }
+
+  handleVideoInit = () => {}
+  handleStartRecord = console.log
+  handleFinishRecord = console.log
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="recorder">
+          <video ref={this.recorder} />
+        </div>
       </div>
     );
   }
