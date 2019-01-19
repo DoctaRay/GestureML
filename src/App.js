22fc72a491a29fef4e6c7aab8705f6b4121a713f
Raiyan Sayeed <yoboi7585@gmail.com>
2019-01-19 10:50:41 -0500

    Switched to 'react-recorder' library (attempt 1)

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import React, { Component } from 'react';
-import videojs from 'video.js';
-
-import 'videojs-record/dist/videojs.record.js';
-import 'webrtc-adapter';
-import 'recordrtc';
+import Recorder from "react-recorder";
 
 // import logo from './logo.svg';
 // import './App.css';
 
-const config = {
-    controls: true,
-    width: 640,
-    height: 480,
-    fluid: false,
-    plugins: {
-        record: {
-            audio: false,
-            video: true,
-            maxLength: 10,
-            debug: true
-        }
-    }
-};
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // Reference to element in the DOM tree.
-    this.recorder = React.createRef();
-  }
-
-  componentDidMount() {
-    this.player = videojs(this.recorder, config, this.handleVideoInit);
-    this.player.on('startRecord', this.handleStartRecord);
-    this.player.on('stopRecord', this.handleFinishRecord)
-  }
-
-  componentWillUnmount() {
-    const { player } = this;
-    if (player) player.dispose();
-  }
-
-  handleVideoInit = () => {}
-  handleStartRecord = console.log
-  handleFinishRecord = console.log
-
+  handleStop = console.log
 
   render() {
     return (
       <div className="app">
-        <div className="recorder">
-          <video ref={this.recorder} />
+        <div className="recorder-container">
+          <Recorder onStop={this.handleStop} />
         </div>
       </div>
     );
