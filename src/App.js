b975498b8faef26bf5ab1dc62f8e8cf20bb881bf
Steven Xie <dev@stevenxie.me>
2019-01-19 11:39:19 -0500

    First working version with VideoJS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import Recorder from "react-recorder";
 
 // import logo from './logo.svg';
 // import './App.css';
 
+const options = {
+  controls: true,
+  width: 320,
+  height: 240,
+  fluid: false,
+  controlBar: {
+    volumePanel: false
+  },
+  plugins: {
+    record: {
+      audio: false,
+      video: true,
+      maxLength: 10,
+      debug: true
+    }
+  }
+};
+
 class App extends Component {
-  handleStop = console.log
+  componentDidMount() {
+    const { videojs } = window;
+    this.player = videojs('recorder', options);
+
+    // error handling
+    this.player.on('deviceError', function() {
+      console.warn('device error:', this.player.deviceErrorCode);
+    });
+    this.player.on('error', function(error) {
+      console.log('error:', error);
+    });
+    // user clicked the record button and started recording
+    this.player.on('startRecord', this.handleStart);
+    // user completed recording and stream is available
+    this.player.on('finishRecord', this.handleFinish);
+  }
+
+  handleStart = console.log;
+  handleFinish = console.log;
+
+  componentWillUnmount() {
+    const { player } = this;
+    if (player) player.dispose();
+  }
 
   render() {
     return (
       <div className="app">
-        <div className="recorder-container">
-          <Recorder onStop={this.handleStop} />
-        </div>
+        <video id="recorder" className="video-js vjs-default-skin" />
       </div>
     );
   }
